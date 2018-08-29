7db2ee20f5acea4f7ce0fb38036b816ed8c33891
Roman Heinrich <roman.heinrich@gmail.com>
2018-08-29 12:40:15 +0200

    converter (wip)

---
+++ lib/recorder/converter.ex
@@ -0,0 +1,53 @@
+defmodule Recorder.RequestConverter do
+  def to_json(method, url, body, headers) do
+    %{
+      url: url,
+      method: method,
+      body: body,
+      headers: headers |> headers_to_json()
+    }
+  end
+
+  def from_json(json) do
+    asmap =
+      json
+      |> Poison.decode!(%{keys: :atoms!})
+
+    asmap
+    |> Map.put(:body, asmap.body)
+    |> Map.put(:headers, asmap.headers |> headers_from_json())
+  end
+
+  def headers_to_json(headers) when is_list(headers) do
+    headers |> Enum.map(fn {k, v} -> [k, v] end)
+  end
+
+  def headers_from_json(headers) when is_list(headers) do
+    headers |> Enum.map(fn [k, v] -> {k, v} end)
+  end
+end
+
+defmodule Http.ResponseConverter do
+  def to_json(payload = %HTTPoison.Response{}) do
+    payload
+    |> Map.put(:headers, payload.headers |> headers_to_json())
+  end
+
+  def from_json(json) do
+    asmap =
+      json
+      |> Poison.decode!(%{keys: :atoms!})
+
+    asmap
+    |> Map.put(:headers, asmap.headers |> headers_from_json())
+    |> Map.put(:body, asmap.body |> Poison.encode!())
+  end
+
+  def headers_to_json(headers) when is_list(headers) do
+    headers |> Enum.map(fn {k, v} -> [k, v] end)
+  end
+
+  def headers_from_json(headers) when is_list(headers) do
+    headers |> Enum.map(fn [k, v] -> {k, v} end)
+  end
+end
--- test/recorder_test.exs
+++ test/recorder_test.exs
@@ -1,8 +1,4 @@
 defmodule RecorderTest do
   use ExUnit.Case
   doctest Recorder
-
-  test "greets the world" do
-    assert Recorder.hello() == :world
-  end
 end
