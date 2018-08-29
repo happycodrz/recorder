cba78042eb0a466a84966fc675faa090166c3c44
Roman Heinrich <roman.heinrich@gmail.com>
2018-08-29 12:40:04 +0200

    a tentative top-level api (wip)

--- lib/recorder.ex
+++ lib/recorder.ex
@@ -1,18 +1,16 @@
 defmodule Recorder do
   @moduledoc """
-  Documentation for Recorder.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Recorder.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  ## recording to a file
+  Recorder.store_in("test/fixtures/something.json") do
+    SomeHTTPClient.make_request1(args)
+    SomeHTTPClient.make_request2(args)
   end
+
+
+  ## this uses the recorded file
+  Recorder.use_fixture("test/fixtures/something.json") do
+    assert SomeHTTPClient.make_request1(args) == %{body: "", headers: [], status_code: 200, url: ""}
+    assert SomeHTTPClient.make_request2(args) == %{body: "", headers: [], status_code: 200, url: ""}
+  end
+  """
 end
